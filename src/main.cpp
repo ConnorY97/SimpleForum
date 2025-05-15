ab295a3defaba6d2eee862a7f262c265ff0c8e6e
Connor Young <c_young97@live.com>
2025-05-15 22:33:49 +1000

    Landing Page.

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@ int main()
 {
     crow::SimpleApp app;
 
-    crow::mustache::set_base("templates");
-
-    CROW_ROUTE(app, "/")([] {
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
         auto page = crow::mustache::load_text("fancypage.html");
         return page;
-        });
+    });
 
     app.port(18080).multithreaded().run();
-
 }
