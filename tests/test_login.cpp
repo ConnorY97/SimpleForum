77b4e535238037abd9fa51ed174feb8e6dbacb4e
Connor Young <c_young97@live.com>
2025-05-17 22:36:42 +1000

    Added simple unit test. Mainly working on layout!

---
+++ tests/test_login.cpp
@@ -0,0 +1,26 @@
+#include "crow.h"
+#include "../src/routes/LoginRoute.h"
+#include "../src/services/UserService.h"
+#include <cassert>
+#include <iostream>
+
+int main() {
+    crow::SimpleApp app;
+    UserService userService;
+
+    crow::mustache::set_base("templates");
+
+    setupLoginRoutes(app, userService);
+
+    // === Test GET /login with no error param
+    {
+        crow::response res(200);
+        res.set_header("Location", "/login");
+
+        assert(res.code == 200);
+        assert(res.body.find("Incorrect username or password") == std::string::npos);
+        std::cout << "[PASS] GET /login (no error)\n";
+    }
+
+    return 0;
+}
