4bc5ed419a4516c3558f7c7c0f2217f95e6ee972
Connor <c_young97@live.com>
2025-05-27 14:00:40 +1000

    Starting to implement comments

---
+++ src/utils/CommentUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "iostream"
+#include <string>
+struct Comment {
+    int id;
+    int forumId;
+    std::string username;
+    std::string comment;
+    std::string createdAt;
+};
