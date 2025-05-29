dd881ef981b51b7ec1102b6cdf2b78800dc8e77b
Connor <c_young97@live.com>
2025-05-29 20:13:36 +1000

    Added final comment test

--- tests/test_comments.cpp
+++ tests/test_comments.cpp
@@ -2,8 +2,10 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "../src/services/CommentService.h"
+#include "../src/services/ForumService.h"
+#include "../src/utils/CommentUtils.h"
 
-TEST_CASE("Create a new comment successfully", "[CommentService]")
+TEST_CASE("Create a new comment", "[CommentService]")
 {
 	CommentService commentService;
 	std::string error;
@@ -14,6 +16,31 @@ TEST_CASE("Create a new comment successfully", "[CommentService]")
 	std::string user = "Test";
 
 	bool result = commentService.addComment(forumId, user, "This is a new comment", error);
+
+	REQUIRE(result);
+	REQUIRE(error.empty());
+}
+
+TEST_CASE("List Comments", "[CommentService]")
+{
+	CommentService commentService;
+	ForumService forumService;
+	std::string error;
+
+	REQUIRE(commentService.clearComments(error));
+	REQUIRE(forumService.clearForums(error));
+
+	int forumId;
+	bool result = forumService.createForum("CommentTest", "This is testing comments", "Test", forumId, error);
+
+	REQUIRE(result);
+
+	result = commentService.addComment(forumId, "TestUser", "Test comment", error);
+
+	REQUIRE(result);
+
+	std::vector<Comment> comments;
+	result = commentService.getCommentsForForum(forumId, comments, error);
 
 	REQUIRE(result);
 	REQUIRE(error.empty());
